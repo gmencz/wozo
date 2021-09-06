a68b6b5cb1883ac7027a503d96356e3ac60ce864
Gabriel Méndez <yo@gabrielmendezc.com>
2021-09-06 13:20:38 +0200

    Add giraphql

--- api/README.md
+++ api/README.md
@@ -1,4 +1,1 @@
-# maisqueauga-capacity-tracker
-
-This program takes a screenshot on start-up and every 30th minute of the [capacity](https://maisqueauga.com/es/aforo-2/) of all
-gyms and saves it in `screenshots/`.
+# Wozo API
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world");
+import { ApolloServer } from "apollo-server";
+import SchemaBuilder from "@giraphql/core";
+
+const builder = new SchemaBuilder({});
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string(),
+      },
+      resolve: (parent, { name }) => `hello, ${name || "World"}`,
+    }),
+  }),
+});
+
+new ApolloServer({ schema: builder.toSchema({}) }).listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
